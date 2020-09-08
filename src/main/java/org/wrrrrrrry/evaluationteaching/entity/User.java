57034d73e14159896b5431ea3ebeebb6fc92f865
lengthh1 <849096252@qq.com>
2020-09-08 16:34:47 +0800

    建立实体

--- src/main/java/org/wrrrrrrry/evaluationteaching/entity/User.java
+++ src/main/java/org/wrrrrrrry/evaluationteaching/entity/User.java
@@ -2,8 +2,19 @@ package org.wrrrrrrry.evaluationteaching.entity;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class User {
     private int id;
     private String name;
+    private String password;
+    private int in_id;
+    private String code;
+    private String sex;
+    private int age;
+    private int height;
+    private String addr;
+    private String intr;
+    private List<Power> powers;
 }
